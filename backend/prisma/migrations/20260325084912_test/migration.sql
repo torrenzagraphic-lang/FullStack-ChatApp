d42d7e2ee5a4e30bea9825d358eff9a3fcde9b62
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-03-25 14:23:35 +0530

    feat: add Prisma schema, database connection, and initial migration setup

---
+++ backend/prisma/migrations/20260325084912_test/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import "dotenv/config"
+import pg, { Pool } from "pg"
+const {Pool} = pg;
+import {PrismaPg} from '@prisma/adapter-pg'
+import {PrismaClient} from '@prisma/client'
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString});
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({adapter});
