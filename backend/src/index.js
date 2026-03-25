a044e047699093ee2fb06cafa68b9728bd0d5d1b
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-03-25 12:07:25 +0530

    Format index.js for consistency and remove redundant code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,11 @@
-import express from 'express';
-import "dotenv/config"
-const app = express()
+import express from "express";
+import "dotenv/config";
+const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello From backend");
+});
 
-app.get("/",(req, res)=>{
-    res.send("Hello From backend")
-})
-
-
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
-})
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
