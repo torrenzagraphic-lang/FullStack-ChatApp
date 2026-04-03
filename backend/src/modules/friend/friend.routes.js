a003b940114d3ddc36352f976b61875161ebd96d
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-04-03 14:25:47 +0530

    complete

--- backend/src/modules/friend/friend.routes.js
+++ backend/src/modules/friend/friend.routes.js
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import { requireAuth } from '../../lib/require-auth.js';
-import { discover, listFriends, sendRequest } from './friend.controller.js';
+import { Router } from "express";
+import { requireAuth } from "../../lib/require-auth.js";
+import { discover, listFriends, sendRequest } from "./friend.controller.js";
 
 export const friendRouter = Router();
 
 friendRouter.use(requireAuth);
 
 //send req
-friendRouter.post('/request', sendRequest);
+friendRouter.post("/request", sendRequest);
 // list friend
-friendRouter.get('/list', listFriends);
+friendRouter.get("/list", listFriends);
 //discover
-friendRouter.get('/discover', discover);
+friendRouter.get("/discover", discover);
